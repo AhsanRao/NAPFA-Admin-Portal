875a06e14021b0ee46744f8bdb1327af7a8cd154
AhsanRao <raoahsan110@gmail.com>
2024-06-05 23:33:57 +0500

    api server live url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,4 @@
-const API_BASE_URL = "http://localhost:5002/api";
+// const API_BASE_URL = "http://localhost:5002/api";
+const API_BASE_URL = "http://13.49.228.139:5000/api";
 
 export default API_BASE_URL;

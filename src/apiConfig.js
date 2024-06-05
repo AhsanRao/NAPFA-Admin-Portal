9d4f68a294587d644f8f1ade2f900f005919bcbd
AhsanRao <raoahsan110@gmail.com>
2024-06-05 23:35:04 +0500

    Update apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,3 @@
-// const API_BASE_URL = "http://localhost:5002/api";
-const API_BASE_URL = "http://13.49.228.139:5000/api";
+const API_BASE_URL = "http://localhost:5002/api";
 
 export default API_BASE_URL;
